1ad2bf67521edfee20d953d31b980a20f67fe133
Grod3n <gordonyjhuang@gmail.com>
2024-09-17 09:57:32 -0400

    Finished 8 and added comments to 8. Need to add more fix comments for previous programs

--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -4,15 +4,16 @@ import java.math.*;
 import java.util.*;
 
 /**
- * A program that prints the prompt of how many items you want to purchase
- * and stops to calculate the subtotal, tax, and total when you type 0
+ * A program that prints out 100 lines of 3 numbers per row of random numbers
+ * then it counts where all 3 numbers are equal and prints out the result
  * 
- * @author: G. Huang
+ * @author: Gordon Huang
  */
 
  public class Review8{
     public static void main(String[] args) throws IOException{
 
+        // Initialize Variables
         int intJackpot = 0;
         int intSlot1;
         int intSlot2;
@@ -20,13 +21,14 @@ import java.util.*;
         Random rand = new Random();
 
 
+        // Prints out 1000 rows and keeps count of triples
         for (int i = 0; i < 1000; i++){
 
             intSlot1 = rand.nextInt(9);
             intSlot2 = rand.nextInt(9);
             intSlot3 = rand.nextInt(9);
 
-            System.out.println(intSlot1 + " + " +intSlot2+ " + " + intSlot3);
+            System.out.println(intSlot1 + " " +intSlot2+ " " + intSlot3);
 
             if (intSlot1 == intSlot2 && intSlot2 == intSlot3) {
                 intJackpot++;
@@ -34,6 +36,7 @@ import java.util.*;
 
         }
 
+        // Output
         System.out.println(intJackpot);
     }
 }
